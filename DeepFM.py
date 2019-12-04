b879c69f330a26f96a0386ef3e62b1cf374e7e61
JobQiu <congminqi@gmail.com>
2019-12-04 14:18:29 -0600

    ...

--- DeepFM.py
+++ DeepFM.py
@@ -12,7 +12,7 @@ from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.metrics import roc_auc_score
 from time import time
 from tensorflow.contrib.layers.python.layers import batch_norm as batch_norm
-from yellowfin import YFOptimizer
+#from yellowfin import YFOptimizer
 
 
 class DeepFM(BaseEstimator, TransformerMixin):
@@ -382,4 +382,3 @@ class DeepFM(BaseEstimator, TransformerMixin):
         """
         y_pred = self.predict(Xi, Xv)
         return self.eval_metric(y, y_pred)
-
